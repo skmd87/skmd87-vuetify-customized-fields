873d4ed8a6de19d78c4294f84210b080b8ef0f22
Samer Draidi <s.aldraidi@ingotbrokers.com>
2023-03-06 11:57:32 +0300

    .

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -3,33 +3,28 @@
 		<template #activator="{ on, attrs }">
 			<v-text-field v-model="localValue" readonly v-bind="{ ...propsBus, ...attrs }" v-on="on">
 				<template v-for="(slot, name) in $scopedSlots" #[name]="item">
-			<slot :name="name" v-bind="item"></slot>
-		</template>
+					<slot :name="name" v-bind="item"></slot>
+				</template>
 			</v-text-field>
 		</template>
 		<v-date-picker v-model="localValue" :max="localMax" @input="menu = false">
 			<template v-for="(slot, name) in $scopedSlots" #[name]="item">
-			<slot :name="name" v-bind="item"></slot>
-		</template>
+				<slot :name="name" v-bind="item"></slot>
+			</template>
 		</v-date-picker>
 	</v-menu>
 </template>
 
 <script>
+import field from "../mixins/field";
 export default {
+	mixins: [field],
 	props: {
-		label: {
-			type: String,
-			default: "Date",
-		},
 		noFuture: {
 			type: Boolean,
 			default: false,
 		},
-		value: {
-			type: String,
-			default: "",
-		},
+
 		hours: {
 			type: Boolean,
 			default: false,
@@ -41,14 +36,6 @@ export default {
 		};
 	},
 	computed: {
-		localValue: {
-			get() {
-				return this.value;
-			},
-			set(val) {
-				this.$emit("input", val);
-			},
-		},
 		localMax() {
 			if (this.max) {
 				return this.max;
