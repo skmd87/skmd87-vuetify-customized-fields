750534bfde0e12960253e6d386afab7722708ff7
Samer Draidi <s.aldraidi@ingotbrokers.com>
2023-05-01 15:04:00 +0300

    .

--- src/components/Textarea.vue
+++ src/components/Textarea.vue
@@ -1,10 +1,10 @@
 <template>
-	<v-textarea v-model="localValue" v-bind="propsBus" v-on="$listeners">
+	<v-textarea v-model="localValue" v-bind="propsBus" v-on="$listeners" ref="vTextArea">
 		<template v-for="(slot, name) in $slots" :slot="name">
 			<slot :name="name"></slot>
 		</template>
 		<template v-for="(slot, name) in $scopedSlots" :slot="name" slot-scope="slotData">
-			<slot :name="name" v-bind="slotData"></slot>		
+			<slot :name="name" v-bind="slotData"></slot>
 		</template>
 	</v-textarea>
 </template>
