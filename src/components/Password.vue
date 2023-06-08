704dd38170fee71162bba81606aded3c0b51f5e5
Samer Draidi <s.aldraidi@ingotbrokers.com>
2023-06-08 16:56:49 +0300

    .

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -1,33 +1,40 @@
 <template>
 	<div :class="'password-' + _uid">
-		<v-tooltip top :value="showTooltip">
-			<template #activator="{ on }">
-				<div class="vc-password-container">
-					<v-text-field
-						v-bind="propsBus"
-						v-model="localPassword"
-						:dummy="on"
-						:append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-						:type="showPassword ? 'text' : 'password'"
-						:rules="calculatedRules"
-						:label="label || 'Password'"
-						counter
-						@click:append="showPassword = !showPassword"
-						@focus="focusHandler"
-						@blur="blurHandler"
-					>
-						<template v-for="(slot, name) in $slots" :slot="name">
-							<slot :name="name"></slot>
-						</template>
-						<template v-for="(slot, name) in $scopedSlots" :slot="name" slot-scope="slotData">
-							<slot :name="name" v-bind="slotData"></slot>
-						</template>
-					</v-text-field>
-					<v-sheet height="4" color="transparent" class="strength-bar">
-						<v-progress-linear v-show="showTooltip" :value="progress" :color="color" rounded></v-progress-linear>
-					</v-sheet>
-				</div>
-			</template>
+		<div class="vc-password-container">
+			<v-text-field
+				:id="'password-field-' + _uid"
+				v-bind="propsBus"
+				v-model="localPassword"
+				:append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+				:type="showPassword ? 'text' : 'password'"
+				:rules="calculatedRules"
+				:label="label || 'Password'"
+				counter
+				ref="input"
+				@click:append="showPassword = !showPassword"
+				@focus="focusHandler"
+				@blur="blurHandler"
+			>
+				<template v-for="(slot, name) in $slots" :slot="name">
+					<slot :name="name"></slot>
+				</template>
+				<template v-for="(slot, name) in $scopedSlots" :slot="name" slot-scope="slotData">
+					<slot :name="name" v-bind="slotData"></slot>
+				</template>
+				<template #append>
+					<v-progress-circular size="24" v-show="showTooltip" :value="progress" :color="color" rounded class="me-2">
+						<span class="text-caption">{{ progress }}%</span>
+					</v-progress-circular>
+					<v-icon v-if="showPassword" @click="showPassword = !showPassword">mdi-eye-off</v-icon>
+					<v-icon v-else @click="showPassword = !showPassword">mdi-eye</v-icon>
+				</template>
+			</v-text-field>
+			<!-- <v-sheet color="transparent" class="strength-bar" :style="{ right: trimSides, left: trimSides  }">
+				<v-progress-linear height="2" v-show="showTooltip" :value="progress" :color="color" rounded></v-progress-linear>
+			</v-sheet> -->
+		</div>
+
+		<v-tooltip top :value="showTooltip" :activator="'#password-field-' + _uid">
 			<template v-if="!noStrength">
 				<div>
 					<v-icon v-if="hasLowerCase" left color="success">{{ mdiCheck }}</v-icon>
@@ -84,6 +91,7 @@ export default {
 			mdiEyeOff,
 			mdiClose,
 			mdiCheck,
+			trimSides: 0,
 		};
 	},
 	computed: {
@@ -150,6 +158,9 @@ export default {
 		},
 	},
 	created() {},
+	mounted() {
+		this.detectBorderRadius();
+	},
 	methods: {
 		showFeedback({ suggestions, warning }) {
 			console.log("🙏", suggestions);
@@ -168,6 +179,17 @@ export default {
 
 			this.showTooltip = false;
 		},
+
+		detectBorderRadius() {
+			setTimeout(() => {
+				console.log(this.$refs.input);
+				//get computed style
+				const element = this.$refs.input.$el;
+				const cssObj = window.getComputedStyle(element, null);
+				console.log("🚀 ~ file: Password.vue:183 ~ setTimeout ~ cssObj:", cssObj);
+				this.trimSides = cssObj.getPropertyValue("border-radius");
+			}, 5000);
+		},
 	},
 };
 </script>
@@ -185,9 +207,16 @@ export default {
 		right: 0;
 		bottom: 17px;
 	}
+	.rounded-lg {
+		left: 8px;
+		right: 8px;
+	}
 
 	.v-text-field.v-input--dense ~ .strength-bar {
 		bottom: 13px;
+	}
+	.v-text-field.v-input--dense.v-text-field--outlined ~ .strength-bar {
+		bottom: 30px;
 	}
 }
 </style>
