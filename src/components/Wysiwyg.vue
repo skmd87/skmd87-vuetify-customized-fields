86c3cc25232eac77771e616c2d6e5da1f5fa3435
skmd87 <36755337+skmd87@users.noreply.github.com>
2023-06-17 10:21:52 +0300

    fix rtl

--- src/components/Wysiwyg.vue
+++ src/components/Wysiwyg.vue
@@ -64,11 +64,61 @@
 <script>
 import { Editor, EditorContent } from "@tiptap/vue-2";
 import TextAlign from "@tiptap/extension-text-align";
-import TextDirection from "tiptap-text-direction-extension";
+// import TextDirection from "tiptap-text-direction-extension";
 import Link from "@tiptap/extension-link";
 import StarterKit from "@tiptap/starter-kit";
 import field from "../mixins/field";
+import { Extension } from "@tiptap/core";
 
+const TextDirection = Extension.create({
+	name: "textDirection",
+	addOptions() {
+		return {
+			types: ["heading", "paragraph"],
+			directions: ["ltr", "rtl", "auto"],
+			defaultDirection: "auto",
+		};
+	},
+	addGlobalAttributes() {
+		return [
+			{
+				types: this.options.types,
+				attributes: {
+					dir: {
+						default: this.options.defaultDirection,
+						parseHTML: (element) => {
+							if (element.attributes.dir && this.options.directions.includes(element.attributes.dir)) {
+								return element.attributes.dir.value;
+							} else {
+								return this.options.defaultDirection;
+							}
+						},
+						renderHTML: (attributes) => {
+							return { dir: attributes.dir };
+						},
+					},
+				},
+			},
+		];
+	},
+	addCommands() {
+		return {
+			setTextDirection:
+				(direction) =>
+				({ commands }) => {
+					if (!this.options.directions.includes(direction)) {
+						return false;
+					}
+					return this.options.types.every((type) => commands.updateAttributes(type, { dir: direction }));
+				},
+			unsetTextDirection:
+				() =>
+				({ commands }) => {
+					return this.options.types.every((type) => commands.resetAttributes(type, "dir"));
+				},
+		};
+	},
+});
 export default {
 	components: {
 		EditorContent,
